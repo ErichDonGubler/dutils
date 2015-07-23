a42cfaa35902617f318b48c5d30e05add12fa74c
Erich Gubler <erichdongubler@gmail.com>
2015-07-23 15:14:31 -0600

    Added some data structures

---
+++ hashset.d
@@ -0,0 +1,30 @@
+struct HashSet(T)
+{
+private:
+	bool[T] setData;
+
+public:
+	bool has(const ref T t) const
+	{
+		return (t in setData) !is null;//XXX: Huh?
+	}
+
+	void add(const ref T t)
+	{
+		setData[t] = true;
+	}
+
+	bool remove(const ref T t)
+	{
+		auto thisHas = this.has(t);
+		if(thisHas)
+			setData.remove(t);
+		return thisHas;
+	}
+
+	void removeAll()
+	{
+		foreach(k; setData.byKey)
+			setData.remove(k);
+	}
+}
---
+++ stack.d
@@ -0,0 +1,48 @@
+import std.exception : enforce;
+
+public struct Stack(T)// Unfortunately, D can't return tuples yet...
+{
+private:
+	T[] ts;
+
+public:
+	void push(T t)
+	{
+		ts ~= t;
+	}
+
+	ref T peek(size_t depth = 0)
+	{
+		auto index = ts.length - depth;
+		enforce(index > 0, "Attempted to peek beyond stack range");
+		return ts[index - 1];
+	}
+
+	ref const(T) readOnlyPeek(size_t depth = 0) const
+	{
+		auto index = ts.length - depth;
+		enforce(index > 0, "Attempted to peek beyond stack range");
+		return ts[index - 1];
+	}
+
+	T pop()//XXX: Unsafe if something throws an exception while getting popped, but acceptable for now
+	{
+		enforce(ts.length > 0, "Attempted to pop empty stack");
+		auto temp = peek;
+		ts.length--;
+		return temp;
+	}
+
+	size_t size() @property const { return ts.length; }
+
+	bool empty() @property const { return ts.length == 0; }
+
+	string toString(string delegate(T) f)
+	{
+		string s = "[";
+		s ~= f(pop);
+		while(!empty)
+			s ~= ", " ~ f(pop);
+		return s ~ "]";
+	}
+}
