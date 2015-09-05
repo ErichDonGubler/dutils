a8914f9ebacf78aac042739f69382b14585d79da
Erich Gubler <erichdongubler@gmail.com>
2015-09-04 18:12:15 -0600

    Add some hash code features to avoid the default of using aggregate addresses

--- hashset.d
+++ hashset.d
@@ -1,26 +1,58 @@
 module dutils.hashset;
 
-struct HashSet(T)
+struct HashSet(T, alias evalFun = null)
 {
 private:
-	bool[T] setData;
+	debug pragma(msg, typeof(evalFun));
+	static if(is(typeof(evalFun) == typeof(null)))
+	{
+		import std.traits : isAggregateType;
+		static if(isAggregateType!T && __traits(hasMember, T, "hashCode"))
+		{
+			debug pragma(msg, "Found hashCode");
+			auto getHashCode(T t) const { return t.hashCode; }
+			alias ourEvalFun = getHashCode;
+		}
+		else
+		{
+			debug pragma(msg, "Using identity");
+			auto identity(T t) const { return t; }
+			alias ourEvalFun = identity;
+		}
+	}
+	else
+	{
+		debug pragma(msg, "Using user-defined hashEval");
+		alias ourEvalFun = evalFun;
+	}
+	debug pragma(msg, typeof(ourEvalFun));
+	import std.traits : ReturnType;
+	alias E = ReturnType!ourEvalFun;
+	bool[E] setData;
+
+	bool hasEvaluated(E e) const
+	{
+		return (e in setData) !is null;//XXX: Huh?
+	}
 
 public:
-	bool has(const ref T t) const
+
+	bool has(T t) const
 	{
-		return (t in setData) !is null;//XXX: Huh?
+		return hasEvaluated(ourEvalFun(t));
 	}
 
-	void add(const ref T t)
+	void add(T t)
 	{
-		setData[t] = true;
+		setData[ourEvalFun(t)] = true;
 	}
 
-	bool remove(const ref T t)
+	bool remove(T t)
 	{
-		auto thisHas = this.has(t);
+		auto e = ourEvalFun(t);
+		auto thisHas = this.hasEvaluated(e);
 		if(thisHas)
-			setData.remove(t);
+			setData.remove(e);
 		return thisHas;
 	}
 
@@ -28,5 +60,11 @@ public:
 	{
 		foreach(k; setData.byKey)
 			setData.remove(k);
+	}
+
+	string toString()
+	{
+		import std.conv : to;
+		return setData.to!string;
 	}
 }
